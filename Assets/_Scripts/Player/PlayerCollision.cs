3a128a7779600ba6e8558512493e78168857b6d3
MuhammetInce <muhammetinceee@gmail.com>
2022-04-27 13:22:06 +0300

    + Add : Add Player Collision Manager for All Collision

---
+++ Assets/_Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,90 @@
+using System;
+using MuhammetInce.Helpers;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    [Header("Floats"), Space] 
+    [SerializeField] private float rotateAroundDuration;
+
+    [Header("Material"), Space] 
+    [SerializeField] private Material balloonHoloMat;
+    [SerializeField] private Material redMat;
+    [SerializeField] private Material blueMat;
+    [SerializeField] private Material yellowMat;
+    [SerializeField] private Material greenMat;
+
+    [Header("Booleans"), Space] 
+    [SerializeField] private bool rotateSide;
+
+
+    // Properties
+    private Renderer PlayerRenderer => GetComponent<Renderer>();
+
+    private Material PlayerMat
+    {
+        get => PlayerRenderer.material;
+        set => PlayerRenderer.material = value;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "ColorChanger":
+                ColorChangeChecker(other);
+                break;
+            case "Balloon":
+                BalloonBurstChecker(other);
+                break;
+        }
+    }
+
+    private void ColorChangeChecker(Collider other)
+    {
+        if (rotateSide)
+        {
+            HelperUtils.RotateAround(gameObject, rotateAroundDuration, 1);
+            rotateSide = false;
+        }
+        else
+        {
+            HelperUtils.RotateAround(gameObject, rotateAroundDuration, -1);
+            rotateSide = true;
+        }
+        gameObject.layer = other.gameObject.layer;
+        PlayerMatChanger();
+    }
+
+    private void BalloonBurstChecker(Collider other)
+    {
+        if (gameObject.layer == other.gameObject.layer)
+        {
+            // TODO
+            // Balloon Burst with Shader
+        }
+        else
+        {
+            other.GetComponent<Renderer>().material = balloonHoloMat;
+        }
+    }
+
+    private void PlayerMatChanger()
+    {
+        switch (gameObject.layer)
+        {
+            case 6:
+                PlayerMat = blueMat;
+                break;
+            case 7:
+                PlayerMat = redMat;
+                break;
+            case 8:
+                PlayerMat = yellowMat;
+                break;
+            case 9:
+                PlayerMat = greenMat;
+                break;
+        }
+    }
+}
