5863d1ac488dd1c3f543aa27c4c7805780c3b2c5
MuhammetInce <muhammetinceee@gmail.com>
2022-04-27 13:21:44 +0300

    + Edit : Just Better Visual Code

--- Assets/_Scripts/HelperUtils.cs
+++ Assets/_Scripts/HelperUtils.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 
 namespace MuhammetInce.Helpers
 {
     public static class HelperUtils
     {
-        
+        public static void RotateAround(GameObject obj, float durationSecond, int rotateSide)
+        {
+            obj.transform.DORotate(new Vector3(0, 0,  obj.transform.eulerAngles.z + 180 * rotateSide), durationSecond, RotateMode.FastBeyond360);
+        }
     }
 }
